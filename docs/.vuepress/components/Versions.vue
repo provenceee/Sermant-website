cd6e6f86bcc348ef3522dcd08b0f9b966ec2213a
provenceee <83857838+provenceee@users.noreply.github.com>
2023-04-17 15:40:13 +0800

    *

--- docs/.vuepress/components/Versions.vue
+++ docs/.vuepress/components/Versions.vue
@@ -21,7 +21,7 @@ export default {
   created: async function() {
     try {
       let response = await Axios.get(
-        'https://api.github.com/repos/huaweicloud/Sermant-website/git/trees/main',
+        'https://api.github.com/repos/provenceee/Sermant-website/git/trees/test1',
       );
       const versionsNode = response.data.tree.find(e => {
         return e.path == 'versions.json';
