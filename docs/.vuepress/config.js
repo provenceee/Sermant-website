7d3aa3632b0278cb5e4adc6772394eed87979333
provenceee <83857838+provenceee@users.noreply.github.com>
2023-04-19 15:06:49 +0800

    *

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: `${process.env.VERSIONS_PATH}`,
+  base: `/Sermant-website${process.env.VERSIONS_PATH}`,
   head: [
       [
         'link', { rel: 'icon', href: '/img/logo.svg'}
